94b938b0a7edb091533394f2518a4e921f4f8109
thecobsb <thecobsb@gmail.com>
2023-02-22 13:46:46 -0300

    Unittest com TDD

---
+++ baconcomovos.py
@@ -0,0 +1,27 @@
+"""
+1 - Receber um número inteiro
+2 - Saber se o número é múltiplo de 3 e 5:
+    Bacon com ovos
+3 - Saber se o número NÃO é múltiplo nem de 3 nem de 5:
+    Passar fome
+4 - Saber se o número é múltiplo somente de 3:
+    Bacon
+5 - Saber se o número é múltiplo somente de 5:
+    Ovos
+
+"""
+
+
+def bacon_com_ovos(n):
+    assert isinstance(n, int), 'n deve ser int'
+
+    if n % 3 == 0 and n % 5 == 0:
+        return 'Bacon com ovos'
+
+    if n % 3 == 0:
+        return 'Bacon'
+
+    if n % 5 == 0:
+        return 'Ovos'
+
+    return 'Passar fome'
